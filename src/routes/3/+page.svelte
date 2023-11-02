a8cf1f299e5e59262b037359ad69b6a819a89149
gacha1987 <daisuke.ogata.work@gmail.com>
2023-11-02 18:17:49 +0900

    問題修正

--- src/routes/3/+page.svelte
+++ src/routes/3/+page.svelte
@@ -1,6 +1,6 @@
 <script>
-    // TODO 課題1 クリックしてカウントが増えるごとにpタグ内の値が反映されるようにしてください。
-    // TODO 課題2 countが10以上になったらalertを出しでください
+    // TODO 課題1 クリックしてカウントが増えるごとにpタグ内のdoubled値がcountの2倍の値で反映されるようにしてください。
+    // TODO 課題2 countが10以上になったらalertを出してcountを0にリセットしてください
     let count = 0;
     let doubled = 0;
     function increment() {
